598ed6396945f770d09248c5eab119a5e932f26b
Poroszkai Attila <poroszkai.attila@live.com>
2016-09-11 21:07:00 +0200

    lerobbantam button

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
 
     <title>Geolocation</title>
 </head>
@@ -17,7 +18,7 @@
             <p>A oldal helyes működéséhez kapcsold be mobilodon a "location" szolgáltatást!</p>
             <h3 class="title">Keressd meg Budapest nevezetességeit!</h3>
             <p>Mond el, hol vagy!</p>
-            <button id="get_location" class="btn btn-success btn-lg">Itt vagyok!</button>
+            <button id="get_location" class="btn btn-success btn-lg btn-lerobbantam"><span class="glyphicon glyphicon-road" aria-hidden="true"></span>  Lerobbantam!</button>
 
 <!--            <input type="text" id="map-search-autocomplete" class="map-search-autocomplete">-->
         </div>
