d05587c9e8d65b2f5547b3a8bd0288de30ade62d
Poroszkai Attila <poroszkai.attila@live.com>
2016-09-11 20:49:20 +0200

    add bootstrap js cdn

--- index.php
+++ index.php
@@ -39,6 +39,7 @@
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="app.js"></script>
-    <!-- <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyArA88vEieAl1ZkFauexoPb2TyKApXLZ4A&libraries=places&callback=initMap"></script> -->
-</body>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
 </html>
